0d3eea2a044f09805ac67546b15efaf29a7cbfee
Nestor <nestor.venegas95@gmail.com>
2021-03-15 12:34:47 -0500

    commit tree c1a3e56e2b9b8a496d8c01fff6978ec3523e5e17

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is mhmm"
+    end
+
+    get'/hometown' do 
+        "My hometown is yikes"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is oof"
+    end
+
 end
